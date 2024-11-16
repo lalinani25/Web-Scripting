4cb5b2f9929e64c14cccf2cae8c11a65e4dbc6e8
Laura Alinani <lalinani@eagles.bridgewater.edu>
2024-11-16 17:56:56 -0500

    Update

--- coursework/javascript/a9.js
+++
@@ -1,7 +0,0 @@
-import { magnifyingLoop} from "./magnifying-loop.js";
-
-let bodyElm = document.querySelector("body")
-
-let magnifier = await magnifyingLoop()
-
-bodyElm.appendChild(magnifier)
--- coursework/javascript/set_anchor_target_blank.js
+++
@@ -1,5 +0,0 @@
-let anchors = document.getElementsByTagName('a');
-
-for (let i = 0; i < anchors.length; i++) {
-    anchors[i].setAttribute('target', '_blank');
-}
